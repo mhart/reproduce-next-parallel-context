9a7297da4c26c24ec077a134bea2de3ed385331e
Michael Hart <mhart@cloudflare.com>
2024-07-12 17:39:23 +1000

    More minimal reproduction

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 To reproduce this error:
 
 1. Run `npm preview` in one tab.
-2. In the next tab `npx playwright test --repeat-each=5`
+2. In the next tab `npm run break`
 3. Observe `TypeError: Cannot read properties of null (reading 'useContext')` in the logs + 500 internal server error
--- src/app/thing2/page.tsx
+++ src/app/thing2/page.tsx
@@ -4,9 +4,5 @@ import { useMyContext } from "../my-context";
 export default function Home() {
   const myString = useMyContext();
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {myString}
-    </main>
-  );
+  return <main>{myString}</main>;
 }
